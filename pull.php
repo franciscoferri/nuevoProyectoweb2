41cff05d1da9eba9b654f0f52c2248a93eeaa241
franciscoferri <114834207+franciscoferri@users.noreply.github.com>
2022-10-02 18:19:11 -0300

    actualizando el primer archivo php

---
+++ pull.php
@@ -0,0 +1,12 @@
+<?php
+
+$persona = [
+               "nombre" => "Perdro"
+               "apellido" => "Sanchez"
+               "edad" => 50
+            ;
+            
+            
+            foreach ($persona as $valor)
+              echo   "$valor <br>";
+              ?>
